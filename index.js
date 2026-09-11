5015e9103bc4de65b3bf2add05fa57b742f07451
Udit <lakshyaudit@gmail.com>
2026-09-12 00:56:51 +0530

    this is the first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+
+const express = require('express');
+
+const app = express();
+const port = d;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/twitter',(req,res)=>{
+    res.send("hopes_alive365")
+})
+
+app.get("/login",(req,res)=>{
+    res.send('<h1>Please login at chai and code </h1>')
+})
+
+app.get('/youtube',(req,res)=>{
+    res.send("<h2>Hi there is Udit</h2>")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
